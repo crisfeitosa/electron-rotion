704b0bd7e1784cb0554743057b9c1c44b8cb7820
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-15 14:16:29 -0300

    feat: add ESLint configuration and fix linting issues
    - Added ESLint script to package.json for code linting.
    - Included @rocketseat/eslint-config and eslint as devDependencies.
    - Updated index.ts to improve code readability and consistency.
    - Changed ts-ignore comments to ts-expect-error in preload/index.ts for better type safety.
    - Fixed formatting in postcss.config.mjs and ensured proper newline at the end of the file.
    - Cleaned up App.tsx and main.tsx for better readability.
    - Updated tailwind.config.js to ensure proper formatting.
    - Refactored tsconfig.json to include stricter compiler options and removed tsconfig.node.json and tsconfig.web.json files.

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,11 +9,11 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? {  } : {}),
+    ...(process.platform === 'linux' ? {} : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
+      sandbox: false,
+    },
   })
 
   mainWindow.on('ready-to-show', () => {
@@ -27,8 +27,8 @@ function createWindow(): void {
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  if (is.dev && process.env.ELECTRON_RENDERER_URL) {
+    mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL)
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,5 @@
+import './styles/global.css'
+
 export function App(): React.JSX.Element {
-  return (
-    <h1 className="text-4xl font-bold text-violet-400">Hello World</h1>
-  )
+  return <h1 className="text-4xl font-bold text-violet-400">Hello World</h1>
 }
