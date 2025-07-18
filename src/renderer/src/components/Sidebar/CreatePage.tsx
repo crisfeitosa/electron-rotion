053c0b6df7c9084862d47d2df4911bc28d724a45
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-18 14:43:13 -0300

    feat: implement document creation functionality in CreatePage component

--- src/renderer/src/components/Sidebar/CreatePage.tsx
+++ src/renderer/src/components/Sidebar/CreatePage.tsx
@@ -1,8 +1,47 @@
+import { useEffect } from 'react'
 import { Plus } from 'phosphor-react'
+import { useNavigate } from 'react-router-dom'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Document } from '@shared/types/ipc'
 
 export function CreatePage() {
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
+  const { isPending: isCreatingNewDocument, mutateAsync: createDocument } =
+    useMutation({
+      mutationFn: async () => {
+        const response = await window.api.createDocument()
+
+        return response.data
+      },
+      onSuccess: (data) => {
+        queryClient.setQueryData<Document[]>(['documents'], (documents) => {
+          return [...(documents || []), data]
+        })
+
+        navigate(`/documents/${data.id}`)
+      },
+    })
+
+  useEffect(() => {
+    function onNewDocument() {
+      createDocument()
+    }
+
+    const unSubscribe = window.api.onNewDocumentRequest(onNewDocument)
+
+    return () => {
+      unSubscribe()
+    }
+  }, [createDocument])
+
   return (
-    <button className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 bg-rotion-800 hover:bg-rotion-700 disabled:opacity-60 z-10">
+    <button
+      onClick={() => createDocument()}
+      disabled={isCreatingNewDocument}
+      className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 bg-rotion-800 hover:bg-rotion-700 disabled:opacity-60 z-10"
+    >
       <Plus className="h-4 w-4" />
       Create new page
     </button>
