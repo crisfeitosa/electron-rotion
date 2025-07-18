f2c75df068b9ab1464e37902f59b57a485d7a98c
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-17 21:17:56 -0300

    feat: wrap Routes component with QueryClientProvider for React Query integration

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,14 @@
 import './styles/global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+
 import { Routes } from './Routes'
 
 export function App(): React.JSX.Element {
-  return <Routes />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+    </QueryClientProvider>
+  )
 }
