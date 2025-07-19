1f5bfc8962402aa445f8d5388be19fbcfce2dabd
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-18 22:40:53 -0300

    fix: update Item component to use onClick for navigation instead of href

--- src/renderer/src/components/Header/Breadcrumbs/Item.tsx
+++ src/renderer/src/components/Header/Breadcrumbs/Item.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import clsx from 'clsx'
+import { useNavigate } from 'react-router-dom'
 
 interface ItemProps {
   isActive?: boolean
@@ -7,11 +8,17 @@ interface ItemProps {
 }
 
 export function Item({ isActive = false, children }: ItemProps) {
+  const navigate = useNavigate()
+
+  function handleNavigate() {
+    navigate('/')
+  }
+
   const Comp = isActive ? 'span' : 'a'
 
   return (
     <Comp
-      href="#"
+      onClick={handleNavigate}
       className={clsx('inline-flex items-center gap-2 hover:text-rotion-50', {
         'text-rotion-50': isActive,
       })}
